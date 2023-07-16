9eb4290a7cc310b8ea8c814393bac4d783226d43
FernandoDLT <96254499+FernandoDLT@users.noreply.github.com>
2023-07-15 18:06:37 -0600

    Added form

---
+++ script.js
@@ -0,0 +1,21 @@
+const form = document.querySelector('form');
+const box = document.querySelector('.box');
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault(); 
+  
+  const username = form.querySelector('input[type="text"]').value;
+  const password = form.querySelector('input[type="password"]').value;
+
+  if (username && password) {
+    form.style.display = 'none';
+    box.style.animation = 'none';
+    box.style.background = 'linear-gradient(350deg, #3ba0ff, #002b8f)';
+
+    const message = document.createElement('h4');
+    message.innerText = 'You are now logged in...';
+
+    box.classList.add('show-message');
+    box.appendChild(message);
+  }
+});
