da8991db2d8d8be647ca0e05acfb3807fabbb371
FernandoDLT <96254499+FernandoDLT@users.noreply.github.com>
2023-09-10 18:39:53 -0500

    fixed form

--- script.js
+++ script.js
@@ -1,21 +1,30 @@
 const form = document.querySelector('form');
 const box = document.querySelector('.box');
+const emailInput = document.getElementById('emailInput');
+
+emailInput.addEventListener('input', function() {
+    if (emailInput.value.trim() !== '') {
+        emailInput.classList.add('has-value');
+    } else {
+        emailInput.classList.remove('has-value');
+    }
+});
 
 form.addEventListener('submit', function(event) {
-  event.preventDefault(); 
-  
-  const username = form.querySelector('input[type="text"]').value;
-  const password = form.querySelector('input[type="password"]').value;
+    event.preventDefault();
 
-  if (username && password) {
-    form.style.display = 'none';
-    box.style.animation = 'none';
-    box.style.background = 'linear-gradient(350deg, #3ba0ff, #002b8f)';
+    const username = form.querySelector('input[type="text"]').value;
+    const email = emailInput.value;
 
-    const message = document.createElement('h4');
-    message.innerText = 'Your email has been sent!';
+    if (username && email) {
+        form.style.display = 'none';
+        box.style.animation = 'none';
+        box.style.background = 'linear-gradient(350deg, #3ba0ff, #002b8f)';
 
-    box.classList.add('show-message');
-    box.appendChild(message);
-  }
+        const message = document.createElement('h4');
+        message.innerText = 'Your email has been sent!';
+
+        box.classList.add('show-message');
+        box.appendChild(message);
+    }
 });
